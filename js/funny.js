0c74b2d0cb85bf91df813820678fb1a957070bbd
umfre <168459037+umfre@users.noreply.github.com>
2024-10-03 05:55:38 -0700

    Add files via upload

---
+++ js/funny.js
@@ -0,0 +1,39 @@
+const body = document.getElementsByTagName('body')
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'Stop tryna skid looserrrrrrrrrrrr :3'
+    }
+    if (event.ctrlKey && event.key === 'C') {
+        event.preventDefault()
+        body[0].innerHTML =
+            'Stop tryna skid looserrrrrrrrrrrr sinister.fun ^_^'
+    }
+    if (event.ctrlKey && (event.key === 'E' || event.key === 'e')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'Stop tryna skid looserrrrrrrrrrrr sinister.fun ^_^'
+    }
+    if (
+        event.ctrlKey &&
+        (event.key === 'I' || event.key === 'i' || event.key === 'Ä\xB1')
+    ) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'Stop tryna skid looserrrrrrrrrrrr sinister.fun ^_^'
+    }
+    if (event.ctrlKey && (event.key === 'K' || event.key === 'k')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.<'
+    }
+    if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
+        event.preventDefault()
+        body[0].innerHTML =
+          'nice try little nigga LOL >.<'
+    }
+})
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
+})
---
+++ js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'connecting :D','Fuck Newgen Larps :3','Sinister Here!!','Sinister.fun :O'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
